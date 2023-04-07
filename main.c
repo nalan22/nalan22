2a66c348135e557f6456c1dff264e9215a15eeaa
Asus Tuf <nalan22@epoka.edu.al>
2023-04-08 00:12:35 +0200

    message

--- main.c
+++ main.c
@@ -25,12 +25,21 @@ double power(double num, int m) {
     return pow(num, m);
 }
 
+double average(double arr[], int n) {
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum / n;
+}
+
 int main() {
     char operator;
     double num1, num2, result;
-    int m;
+    int m, count = 0;
+    double nums[100];
 
-    printf("Enter an operator (+, -, *, /, s, p): ");
+    printf("Enter an operator (+, -, *, /, s, p, a): ");
     scanf("%c", &operator);
 
     switch (operator) {
@@ -69,6 +78,15 @@ int main() {
             scanf("%lf %d", &num1, &m);
             result = power(num1, m);
             printf("%.2lf^%d = %.2lf", num1, m, result);
+            break;
+        case 'a':
+            printf("Enter numbers (type any character to exit): ");
+            while (scanf("%lf", &num1) == 1) {
+                nums[count] = num1;
+                count++;
+            }
+            result = average(nums, count);
+            printf("Average = %.2lf", result);
             break;
         default:
             printf("Error: Invalid operator");
